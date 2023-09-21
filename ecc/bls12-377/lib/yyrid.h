2ff14b626a457eacc6c223bafeac33ba4be58972
Efsane Soyer <efsanesoyer@berkeley.edu>
2023-09-21 05:19:32 +0000

    need to edit g1.go file to run

--- ecc/bls12-377/lib/yyrid.h
+++ ecc/bls12-377/lib/yyrid.h
@@ -3,4 +3,4 @@
 
 
 void* multi_scalar_init_wrapper(void* points, uint64_t len);
-void multi_scalar_mult_wrapper(void* p, void* ctx, void* points, void* scalars, uint64_t len);
+void multi_scalar_mult_wrapper(void* p, void* ctx, void* scalars, uint64_t len);
