36d084ecbb37d6a9f7dbae091184d8c55ba2ba28
= <=>
2024-03-21 23:33:24 +0000

    Swap fft for icicle functions

--- ecc/bls12-377/lib/icicle.h
+++ ecc/bls12-377/lib/icicle.h
@@ -2,3 +2,5 @@
 
 int msm_cuda_bls12_377(
   void* out, void* points, void* scalars, size_t count, unsigned large_bucket_factor, size_t device_id);
+
+int ntt_cuda_bls12_377(void* arr, size_t n, bool inverse, size_t device_id);
